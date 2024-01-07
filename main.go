ef90eafdf3006ce38a4f59e74853b40c9e632b86
Aditya Mayukh Som <adi.kg2@gmail.com>
2024-01-07 20:31:37 +0530

    FIX: local module importing

--- main.go
+++ main.go
@@ -3,15 +3,18 @@ package main
 import (
 	"fmt"
 	"log"
+
+	server "github.com/AdityaMayukhSom/ruskin/server"
+	store "github.com/AdityaMayukhSom/ruskin/store"
 )
 
 func main() {
-	serverconf := &ServerConfig{
-		port:         ":3000",
-		storeFactory: NewMemoryStoreFactory(nil),
+	serverconf := &server.ServerConfig{
+		Port:         ":3000",
+		StoreFactory: store.NewMemoryStoreFactory(nil),
 	}
 
-	server, err := NewServer(serverconf)
+	server, err := server.NewServer(serverconf)
 	if err != nil {
 		log.Fatal(err)
 	}
