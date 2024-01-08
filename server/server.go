136714a55b3ae4a3a1c02784bf4f03ec7752cffb
Aditya Mayukh Som <adi.kg2@gmail.com>
2024-01-08 11:40:13 +0530

    UPDATE: server
    - server now stores a list of producers and consumer
    - starting the server starts producers and consumers too
    - CreateTopic is used to create a new Topic in a running server
    - quitChannel is used to signal the server to stop

--- server/server.go
+++ server/server.go
@@ -1,9 +1,10 @@
 package server
 
 import (
-	"net/http"
+	"fmt"
 
 	store "github.com/AdityaMayukhSom/ruskin/store"
+	transport "github.com/AdityaMayukhSom/ruskin/transport"
 )
 
 type ServerConfig struct {
@@ -13,21 +14,54 @@ type ServerConfig struct {
 
 type Server struct {
 	*ServerConfig
-	topics map[string]store.Store
+	topics      map[string]store.Store
+	producers   []transport.Producer
+	consumers   []transport.Consumer
+	quitChannel chan struct{}
 }
 
-func NewServer(scfg *ServerConfig) (*Server, error) {
-	return &Server{ServerConfig: scfg}, nil
+func NewServer(config *ServerConfig) (*Server, error) {
+	return &Server{
+		ServerConfig: config,
+		producers: []transport.Producer{
+			transport.NewHTTPProducer(
+				config.ListenAddr,
+			),
+		},
+		topics:      make(map[string]store.Store),
+		quitChannel: make(chan struct{}),
+	}, nil
 }
 
-func (s *Server) Start() {
-	http.ListenAndServe(s.ListenAddr, nil)
-}
-
-func (s *Server) CreateTopic(name string) {
+func (s *Server) CreateTopic(name string) bool {
 	_, ok := s.topics[name]
 	if !ok {
 		s.topics[name] = s.StoreFactory.Produce()
+		return true
+	}
+	return false
+}
+
+func (s *Server) Start() error {
+
+	for _, consumer := range s.consumers {
+		err := consumer.Start()
+		if err != nil {
+			// if one consumer is failing, doesn't mean whole
+			// server has to be stopped, so print and move on
+			fmt.Println(err)
+		}
 	}
 
+	for _, producer := range s.producers {
+		err := producer.Start()
+		if err != nil {
+			// if one producer is failing, doesn't mean whole
+			// server has to be stopped, so print and move on
+			fmt.Println(err)
+		}
+	}
+
+	<-s.quitChannel
+	return nil
 }
