a60d4a7ea5adb0b2cf82ddb19b10c7ceddef4379
Aditya Mayukh Som <adi.kg2@gmail.com>
2024-01-13 20:07:04 +0530

    ADD: tint logger for colored logging

--- main.go
+++ main.go
@@ -1,13 +1,31 @@
 package main
 
 import (
-	"log"
+	"log/slog"
+	"os"
+	"time"
 
 	server "github.com/AdityaMayukhSom/ruskin/server"
 	store "github.com/AdityaMayukhSom/ruskin/store"
+
+	tint "github.com/lmittmann/tint"
 )
 
 func main() {
+	// create a new logger
+	tintLogger := slog.New(
+		tint.NewHandler(
+			os.Stdout,
+			&tint.Options{
+				Level:      slog.LevelDebug,
+				TimeFormat: time.DateTime,
+			},
+		),
+	)
+
+	// set global logger with custom options
+	slog.SetDefault(tintLogger)
+
 	serverconf := &server.ServerConfig{
 		ListenAddr:   ":3000",
 		StoreFactory: store.NewMemoryStoreFactory(nil),
@@ -15,11 +33,11 @@ func main() {
 
 	server, err := server.NewServer(serverconf)
 	if err != nil {
-		log.Fatal(err)
+		slog.Error(err.Error())
 	}
 
 	err = server.Start()
 	if err != nil {
-		log.Fatal(err)
+		slog.Error(err.Error())
 	}
 }
