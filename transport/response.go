c1dd85bab1b71ffe11bec6a5aab94a3931137bea
Aditya Mayukh Som <adi.kg2@gmail.com>
2024-01-13 20:08:00 +0530

    CREATED: standardized response to clients

--- transport/response.go
+++ transport/response.go
@@ -6,20 +6,42 @@ import (
 	"net/http"
 )
 
-type TransportResponse struct {
+type PublishedResponse struct {
+	Message string
+	Offset  int
+}
+
+func NewPublishedResponse(message string, offset int) *PublishedResponse {
+	return &PublishedResponse{
+		Message: message,
+		Offset:  offset,
+	}
+}
+
+func (pr PublishedResponse) String() string {
+	return fmt.Sprintf("{\"Message\": %s, \"Offset\": %d}", pr.Message, pr.Offset)
+}
+
+type ErrorResponse struct {
 	Message string
 }
 
-func (tr TransportResponse) String() string {
-	return fmt.Sprintf("{\"Message\": %s}", tr.Message)
+func NewErrorResponse(message string) *ErrorResponse {
+	return &ErrorResponse{
+		Message: message,
+	}
 }
 
-func writeResponse(w http.ResponseWriter, statusCode int, responseMessage string) {
-	w.WriteHeader(http.StatusBadRequest)
-	if respBody, err := json.Marshal(TransportResponse{
-		Message: responseMessage,
-	}); err == nil {
-		w.Header().Set("Content-Type", "application/json")
+func (er ErrorResponse) String() string {
+	return fmt.Sprintf("{\"Message\": %s}", er.Message)
+}
+
+func writeResponse[T fmt.Stringer](w http.ResponseWriter, statusCode int, responseBody *T) {
+	// you need to set headers before setting WriteHeader with status code
+	// refer to https://stackoverflow.com/questions/39273490/adding-a-header-to-a-responsewriter
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(statusCode)
+	if respBody, err := json.Marshal(responseBody); err == nil {
 		w.Write(respBody)
 	}
 }
