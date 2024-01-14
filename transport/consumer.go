c880b67df65a974dd53c446bfb938216c982df51
Aditya Mayukh Som <adi.kg2@gmail.com>
2024-01-14 09:55:46 +0530

    ADD: WSConsumer as implementation to Consumer interface
        - WSConsumer also implements Handler interface for ServeHTTP
          function for passing to http.ListenAndServe
        - WSConsumer having listenAddr for consumer port

--- transport/consumer.go
+++ transport/consumer.go
@@ -1,5 +1,30 @@
 package transport
 
+import (
+	"log/slog"
+	"net/http"
+)
+
 type Consumer interface {
 	Start() error
+}
+
+type WSConsumer struct {
+	listenAddr string
+}
+
+func NewWSConsumer(listenAddr string) *WSConsumer {
+	return &WSConsumer{
+		listenAddr: listenAddr,
+	}
+}
+
+func (c *WSConsumer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (c *WSConsumer) Start() error {
+	slog.Info("websockets consumer started", "port", c.listenAddr)
+
+	return http.ListenAndServe(c.listenAddr, c)
 }
