2111c974822f8852ad2a01568bb64cdfe5384a4b
Somoprovo Bhattacharjee <somoprovobhattacharjee@gmail.com>
2024-01-22 10:57:05 +0530

    ADD: load partition factory

---
+++ load/load_partition_factory.go
@@ -0,0 +1,39 @@
+package replica
+
+import "github.com/AdityaMayukhSom/ruskin/consumer"
+
+type LoadPartitionFactoryConfig struct {
+}
+
+type LoadPartitionFactory struct {
+	*LoadPartitionFactoryConfig
+}
+
+func NewLoadPartitionFactory(config *LoadPartitionFactoryConfig) *LoadPartitionFactory {
+	return &LoadPartitionFactory{
+		LoadPartitionFactoryConfig: config,
+	}
+}
+
+func (lpf *LoadPartitionFactory) Produce(
+	topicChannel <-chan string,
+	consumerChannel <-chan consumer.Consumer,
+) *LoadPartition {
+	// mu.Lock()
+	// defer mu.Unlock()
+
+	// if f == nil {
+	// 	panic("Factory is nil")
+	// }
+	// if _, exist := LoadPartitionFactories[pkgName]; exist {
+	// 	panic("Factory already registered")
+	// }
+
+	// LoadPartitionFactories[pkgName] = f
+
+	return &LoadPartition{
+		topicChannel:    topicChannel,
+		consumerChannel: consumerChannel,
+		relays:          make(map[string]consumer.ConsumerRelay),
+	}
+}
