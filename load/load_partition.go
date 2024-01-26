d5b87037feaee1a189c7ede6acac82a391e2a492
Aditya Mayukh Som <adi.kg2@gmail.com>
2024-01-26 13:20:38 +0530

    FIX: LoadPartition function signatures
    - HandleSubscription now has access to complete Consumer instance rather
      than just the websocket connection object.
    - HandleSubcription adds the consumer to the relay corresponding to the
      topic which the consumer wants to be subscribed to.
    - Fixed consumer channel to adapt to topic less consumer and changed the
      name to connection channel.

--- load/load_partition.go
+++ load/load_partition.go
@@ -3,22 +3,32 @@ package load
 import (
 	consumer "github.com/AdityaMayukhSom/ruskin/consumer"
 	messagequeue "github.com/AdityaMayukhSom/ruskin/messagequeue"
-	websocket "github.com/gorilla/websocket"
 )
 
 type LoadPartition struct {
+	/*
+		Whenever a new message is generated in a topic, that topic is pushed to this
+		channel so th
 
-	// TODO: we need some kind of interface named topic identifier which
-	// can be used to identify topics over a cluster of computers, and then
-	// replace the *messagequeue.Store in topicChannel with that interface
-	// which can be used to return a unique identifier, preferebly a UUID or
-	// IP address with port of the computer in which that port runs, and also
-	// an interface Connector needs to be implemented which can be used to
-	// establish connection both over the nerwork and via pointer if same address
-	// space is used (can be logically same or physically same)
-	topicChannel    <-chan *messagequeue.Store
-	consumerChannel <-chan consumer.Consumer
-	relays          map[*messagequeue.Store]consumer.ConsumerRelay
+		TODO: we need some kind of interface named `TopicIdentifier` which
+		can be used to identify topics over a cluster of computers and then
+		replace the *messagequeue.Store in topicChannel with that interface
+		which can be used to return a unique identifier, preferebly a UUID or
+		IP address with port of the computer in which that topic exists.
+
+		Also an interface `Connector` needs to be implemented which can be used to
+		establish connection both over the nerwork and via pointer if same address
+		space is used (can be either logically same or physically same)
+	*/
+	topicChannel <-chan *messagequeue.Store
+
+	connnectionChannel <-chan struct {
+		consumer *consumer.Consumer
+		topic    *messagequeue.Store
+	}
+
+	// A map to
+	relays map[*messagequeue.Store]consumer.ConsumerRelay
 }
 
 func (lp *LoadPartition) ProcessStream(topicName *messagequeue.Store) {
@@ -29,23 +39,27 @@ func (lp *LoadPartition) ProcessStream(topicName *messagequeue.Store) {
 	go consumerRelay.Relay()
 }
 
-func (lp *LoadPartition) HandleSubscription(conn *websocket.Conn, topic *messagequeue.Store) {
+func (lp *LoadPartition) HandleSubscription(cnsmr *consumer.Consumer, topic *messagequeue.Store) {
 	relay, found := lp.relays[topic]
 	if !found {
 		relay = consumer.NewWSConsumerRelay(topic)
 		// no idea if we need to use mutex for synchronization
 		lp.relays[topic] = relay
 	}
-
+	relay.AddConsumer(cnsmr)
 }
 
 func (lp *LoadPartition) Start() {
 	for {
 		select {
+		// TODO: we need to depend on an interface to give us the TopicIdentifier
+		// rather than depending on the topic channel directly. We can create
+		// an interface LoadPartition with GetTopic() which internally handles
+		// how to get the notification of the arrival of a new message under a topic.
 		case topicName := <-lp.topicChannel:
 			go lp.ProcessStream(topicName)
-		case consumer := <-lp.consumerChannel:
-			go lp.HandleSubscription(consumer.Conn, consumer.Topic)
+		case con := <-lp.connnectionChannel:
+			go lp.HandleSubscription(con.consumer, con.topic)
 		}
 	}
 }
