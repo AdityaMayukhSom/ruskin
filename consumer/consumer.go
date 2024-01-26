8ebd7dccd3f98f76e6ead7f64b52c2fadf91767d
Aditya Mayukh Som <adi.kg2@gmail.com>
2024-01-26 11:46:52 +0530

    FIX: Consumer only contains pointer to the Connection
    Consumer struct does not store the topic to which the consumer is
    connected to, thus we can use the same consumer object to subscribe to
    different ConsumerRelay ensureing same connection is used for different
    topics if they are subscribed together.

--- consumer/consumer.go
+++ consumer/consumer.go
@@ -1,14 +1,11 @@
 package consumer
 
 import (
-	"github.com/gorilla/websocket"
+	websocket "github.com/gorilla/websocket"
 )
 
 // Client is a middleman between the websocket connection and the hub.
 type Consumer struct {
 	// The websocket connection.
 	Conn *websocket.Conn
-
-	// Buffered channel of outbound messages.
-	Topics []string
 }
