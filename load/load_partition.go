ba4e5d9b4831c3bf44df9a480ee5cfe795e4f826
Aditya Mayukh Som <adi.kg2@gmail.com>
2024-01-26 10:33:12 +0530

    FIX: pointer to the Store used as identifier to the topic

--- load/load_partition.go
+++ load/load_partition.go
@@ -2,16 +2,26 @@ package load
 
 import (
 	consumer "github.com/AdityaMayukhSom/ruskin/consumer"
+	messagequeue "github.com/AdityaMayukhSom/ruskin/messagequeue"
 	websocket "github.com/gorilla/websocket"
 )
 
 type LoadPartition struct {
-	topicChannel    <-chan string
+
+	// TODO: we need some kind of interface named topic identifier which
+	// can be used to identify topics over a cluster of computers, and then
+	// replace the *messagequeue.Store in topicChannel with that interface
+	// which can be used to return a unique identifier, preferebly a UUID or
+	// IP address with port of the computer in which that port runs, and also
+	// an interface Connector needs to be implemented which can be used to
+	// establish connection both over the nerwork and via pointer if same address
+	// space is used (can be logically same or physically same)
+	topicChannel    <-chan *messagequeue.Store
 	consumerChannel <-chan consumer.Consumer
-	relays          map[string]consumer.ConsumerRelay
+	relays          map[*messagequeue.Store]consumer.ConsumerRelay
 }
 
-func (lp *LoadPartition) ProcessStream(topicName string) {
+func (lp *LoadPartition) ProcessStream(topicName *messagequeue.Store) {
 	consumerRelay := lp.relays[topicName]
 	// relay over connection can take time, but need to consider if sending the message
 	// actually takes some time, because we aren't waiting for the reply, we are only
@@ -19,23 +29,23 @@ func (lp *LoadPartition) ProcessStream(topicName string) {
 	go consumerRelay.Relay()
 }
 
-func (lp *LoadPartition) HandleSubscription(conn *websocket.Conn, topics []string) {
-	for _, topic := range topics {
-		_, found := lp.relays[topic]
-		if !found {
-			// no idea if we need to use mutex for synchronization
-			lp.relays[topic] = consumer.NewWSConsumerRelay()
-		}
+func (lp *LoadPartition) HandleSubscription(conn *websocket.Conn, topic *messagequeue.Store) {
+	relay, found := lp.relays[topic]
+	if !found {
+		relay = consumer.NewWSConsumerRelay(topic)
+		// no idea if we need to use mutex for synchronization
+		lp.relays[topic] = relay
 	}
+
 }
 
 func (lp *LoadPartition) Start() {
 	for {
 		select {
 		case topicName := <-lp.topicChannel:
-			lp.ProcessStream(topicName)
+			go lp.ProcessStream(topicName)
 		case consumer := <-lp.consumerChannel:
-			lp.HandleSubscription(consumer.Conn, consumer.Topics)
+			go lp.HandleSubscription(consumer.Conn, consumer.Topic)
 		}
 	}
 }
