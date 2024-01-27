b509be9e2155f68c3510a3222f72babc2e49c721
Aditya Mayukh Som <adi.kg2@gmail.com>
2024-01-27 12:06:55 +0530

    ADD: Listen to listen from various message queues

--- load/load_distributor.go
+++ load/load_distributor.go
@@ -1,6 +1,24 @@
 package load
 
+import "fmt"
+
 type LoadDistributor struct {
 	PartitionsCount int
 	Partitions      []LoadPartition
+}
+
+func (ld *LoadDistributor) Listen() {
+	agg := make(chan string)
+	for _, ch := range chans {
+		go func(c chan string) {
+			for msg := range c {
+				agg <- msg
+			}
+		}(ch)
+	}
+
+	select {
+	case msg <- agg:
+		fmt.Println("received ", msg)
+	}
 }
