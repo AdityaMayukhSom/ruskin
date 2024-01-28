8a920d48c816e98bf50cb351496280dad5a3d2e0
Somoprovo Bhattacharjee <somoprovobhattacharjee@gmail.com>
2024-01-29 04:19:58 +0530

    FIX: fixed the load_distribution file

--- load/load_distributor.go
+++ load/load_distributor.go
@@ -2,20 +2,28 @@ package load
 
 import (
 	"fmt"
+	"sync"
 
 	consumer "github.com/AdityaMayukhSom/ruskin/consumer"
 )
 
 // Making a map of TopicId and LoadPartition
 // The map values have to be mapped to LoadPartition uid(will implement later)
-var TopicToPartitionMap = make(map[string]*LoadPartition)
+var TopicToPartitionMap map[string]*LoadPartition
 
 type LoadDistributor struct {
 	PartitionsCount int
 	Partitions      []LoadPartition
+	mutex           sync.RWMutex
 }
 
-func (ld *LoadDistributor) getOrCreatePartition(topic string) *LoadPartition {
+/*Function to get partition and check if the load partition is present or not if
+its present it returns the load partition else it returns after creating a new load partition*/
+
+func (ld *LoadDistributor) GetPartition(topic string) *LoadPartition {
+
+	ld.mutex.Lock()
+	defer ld.mutex.Unlock()
 
 	//Checking if loadPartition exist in the map
 	if partition, exist := TopicToPartitionMap[topic]; exist {
@@ -26,20 +34,23 @@ func (ld *LoadDistributor) getOrCreatePartition(topic string) *LoadPartition {
 	using the load partition factory and then use that factory using the config and then
 	producing the channels*/
 
+	//add the desired config to the partition
 	config := LoadPartitionFactoryConfig{}
 
+	//creating channels for load partition
 	topicChannel := make(chan string)
 	consumerChannel := make(chan consumer.Consumer)
 
+	//creating a new load partition using load partiotion factory
 	factory := NewLoadPartitionFactory(&config)
 	newPartition := factory.Produce(topicChannel, consumerChannel)
 
-	//Adding the partition into the map and then returning
+	//Adding the partition into the map and then returning the new partition
 	TopicToPartitionMap[topic] = newPartition
 	return newPartition
 }
 
-func (ld *LoadDistributor) Listen() {
+func (ld *LoadDistributor) Start() {
 	agg := make(chan string)
 	for _, ch := range chans {
 		go func(c chan string) {
