8e4c2415e5515dff3c512e825a6057024a87354e
Somoprovo Bhattacharjee <somoprovobhattacharjee@gmail.com>
2024-02-09 02:47:53 +0530

    ADD: Producer Broker functionalities are added

--- producer/producer_broker.go
+++ producer/producer_broker.go
@@ -1,1 +1,34 @@
 package producer
+
+import (
+	"fmt"
+
+	"github.com/AdityaMayukhSom/ruskin/messagequeue"
+)
+
+type ProducerBroker interface {
+	Start() error
+	TopictoMessageQueueMap map[string] messagequeue.MessageQueue
+}
+
+// here for testing purposes only one channel is passed
+// we have to actually pass multiple producers channels through it.
+func Start(producerChannels []chan string) error {
+	
+	fmt.Println("Producer Broker is up")
+
+	// Start a goroutine for each producer channel to produce messages to the message queue
+	for _, ch := range producerChannels {
+		go func(channel chan string) {
+			for msg := range channel {
+				err := messageQueue.Produce(msg)
+				if err != nil {
+					fmt.Println("Error producing message:", err)
+				}
+			}
+		}(ch)
+	}
+
+	select {}
+
+}
